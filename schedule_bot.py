e0ecbb5a099d2ad9ff5aedbb594f28676917b326
PaulAntonescu <74125937+PaulAntonescu@users.noreply.github.com>
2021-04-18 21:57:28 -0400

    Update schedule_bot.py

--- schedule_bot.py
+++ schedule_bot.py
@@ -10,12 +10,12 @@ channel = None
 
 # Title, Description, Links, Displayed Time, Thumbnail, military time, Repeat reminders (mon = 0, tues = 1, sun = 6)
 bot_schedule = {
-	1: ["CIS 524 Comparative Programming", "https://csuohio.zoom.us/j/84358174972?pwd=ZzdSU0JtRHU3N2xaakRFUHFwNExBUT09", "ID: 843 5817 4972\nPass: 331464\n10:15am - 11:05am", "https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 9, 50, "024"],
-	2: ["CIS 470 Mobile app development", "https://csuohio.zoom.us/j/85886744984", "ID: UNKNOWN\nPass: NONE\n6:00pm - 7:15pm", "https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 5+12, 50, "02"],
-	3: ["EEC 383 Digital Systems", "https://csuohio.zoom.us/j/84834548200?pwd=alhSK0MzMEsrNDdYdEF1R2piTVhoUT09", "ID: 848 3454 8200\nPass: 611179\n10:00am - 11:15am", "https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 9, 50, "13"],
-	4: ["EEC 494 Senior Design 2.woh", "https://csuohio.zoom.us/j/83270952870?pwd=bHJVazI1R1lJdC9rZzhjUzZiaExOZz09", "ID: 832 7095 2870\nPass: 350450\n12:30pm - 1:45pm", "https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 12, 20, "13"],
-	5: ["EEC 408 Internet Programming", "https://csuohio.zoom.us/j/85184072489", "ID: 851 8407 2489\nPass: NONE\n2:00pm - 3:15pm", "https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 1+12, 50, "13"],
-	6: ["CIS 492 Big Data", "https://csuohio.zoom.us/j/86599894787","ID: 865 9989 4787\nPass: NONE\n4:30pm - 5:45pm","https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 4+12, 20, "02"]
+	1: ["CIS 524 Comparative Programming", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", "ID: 843 5817 4972\nPass: 331464\n10:15am - 11:05am", "https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 9, 50, "024"],
+	2: ["CIS 470 Mobile app development", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", "ID: UNKNOWN\nPass: NONE\n6:00pm - 7:15pm", "https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 5+12, 50, "02"],
+	3: ["EEC 383 Digital Systems", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", "ID: 848 3454 8200\nPass: 611179\n10:00am - 11:15am", "https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 9, 50, "13"],
+	4: ["EEC 494 Senior Design 2.woh", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", "ID: 832 7095 2870\nPass: 350450\n12:30pm - 1:45pm", "https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 12, 20, "13"],
+	5: ["EEC 408 Internet Programming", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", "ID: 851 8407 2489\nPass: NONE\n2:00pm - 3:15pm", "https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 1+12, 50, "13"],
+	6: ["CIS 492 Big Data", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX","ID: 865 9989 4787\nPass: NONE\n4:30pm - 5:45pm","https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 4+12, 20, "02"]
 	# 7: ["TEST", "https://exploreit.info","ID: Yes\nPass: NONE\nRight now","https://www.csuohio.edu/sites/default/files/CSU-Seal-Reversed.png", 7+12, 43, "0123456"]
 }
 
